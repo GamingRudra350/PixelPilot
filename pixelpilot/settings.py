14e4f1ad4e7c5b6d56c046091bc49a129623b2e5
GamingRudra350 <riyanshshori81@gmail.com>
2026-05-12 18:32:40 +0530

    Add allowed hosts for deployment

--- pixelpilot/settings.py
+++ pixelpilot/settings.py
@@ -7,7 +7,11 @@ SECRET_KEY = 'django-insecure-change-this-in-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'pixelpilot-i3zr.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
